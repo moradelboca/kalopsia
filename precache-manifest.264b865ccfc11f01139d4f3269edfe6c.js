70ba02191d6a592578ab291c48253143e2ea7ab2
José Mora <jmora000@outlook.com>
2022-10-17 21:51:12 -0300

    Updates

---
+++ precache-manifest.264b865ccfc11f01139d4f3269edfe6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fc7ad3cbd51cac2366f",
+    "url": "/react/static/css/main.ed0495cb.chunk.css"
+  },
+  {
+    "revision": "7fc7ad3cbd51cac2366f",
+    "url": "/react/static/js/main.7fc7ad3c.chunk.js"
+  },
+  {
+    "revision": "6ef34178273651028d93",
+    "url": "/react/static/js/1.6ef34178.chunk.js"
+  },
+  {
+    "revision": "011079f9cc2d25c428a2",
+    "url": "/react/static/js/2.011079f9.chunk.js"
+  },
+  {
+    "revision": "0a3201a1992358dd7f3a",
+    "url": "/react/static/js/runtime~main.0a3201a1.js"
+  },
+  {
+    "revision": "1727f5c4f28a82b1a3d9cafe2a3f9335",
+    "url": "/react/index.html"
+  }
+];
